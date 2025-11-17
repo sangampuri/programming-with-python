8655a30ab7474c9ac20994ac2f92da0296117a30
Sangam Puri <sangampuree@gmail.com>
2025-11-17 18:41:46 +0545

    working with numbers -> int , float and maths

---
+++ day1-numbers.py
@@ -0,0 +1,14 @@
+
+#!Program to add two numbers using integers
+# x=int(input("Enter x:"))
+# y=int(input("Enter y:"))
+
+# sum = x+y
+# print(sum)
+
+#!Floats
+x=float(input("Enter x:"))
+y=float(input("Enter y:"))
+
+divide = round(x/y,2)
+print(f"{divide:,}")
