5459237425f43ee1e3a1f6eb602df789fbec22de
Sangam Puri <sangampuree@gmail.com>
2025-11-18 20:45:15 +0545

    importing libraries

---
+++ day2-libraries.py
@@ -0,0 +1,22 @@
+
+#!Importing an module
+#import random
+# coin = random.choice(["heads","Tails"])
+# print(coin)
+
+#!importing a specific function from a module
+# from random import choice
+# coin = choice(["heads","Tails"])
+# print(coin)
+
+#!randint() function -> Returns ramdom number between  two numbers
+# number = random.randint(1,10)
+# print(number)
+
+#!random.shjuffle()
+# cards =["jack","queen","king"]
+# random.shuffle(cards)
+# for card in cards:
+#     print(card)
+
+#!sys
